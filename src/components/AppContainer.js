b36deabbfc95cc9961754c3071fef23055eec837
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-03-31 18:21:15 -0300

    Pagina do vendedor finalizada, estrutura e estilizacao.

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
-import Footer from './Footer'
-import Header from './Header'
+import React, { Component } from 'react';
+import PageSeller from './PageSeller';
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import Footer from './Footer';
+import Header from './Header';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      cinza: "#4D4D59",
+      vermelho: "#EF4242",
+      verdeazulado: "#40A59B",
+      amarelo: "#F1BF63",
+      creme: "FFFCEF"
+    },
+  },
+})
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
+        <ChakraProvider theme={theme}>
+          <PageSeller />
+        </ChakraProvider>
         <Footer />
       </div>
     );

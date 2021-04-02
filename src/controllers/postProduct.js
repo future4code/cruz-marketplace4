86dae700716871de4ed2e9de521e7d772f4ba423
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-04-01 21:37:37 -0300

    Implementada a funçao de excluir produtos da lista de produtos cadastrados.

--- src/controllers/postProduct.js
+++ src/controllers/postProduct.js
@@ -4,7 +4,6 @@ import { urlApi } from "./config/apiConfigs";
 export const postProducts = async (body) => {
   try {
     const response = await axios.post(urlApi, body);
-
     return response.status
   } catch (error) {
     return error.response.data.message

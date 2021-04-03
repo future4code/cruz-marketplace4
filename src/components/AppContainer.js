3559e00cfd34f9aa1d5266fc5bd514e89ab64c4a
Fagner Zulin <fagnerzulin11@gmail.com>
2021-04-03 11:35:16 -0300

    Corrigindo alguns problemas com funcionalidades, usabilidade e limpeza do código

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -28,60 +28,11 @@ const Container = styled.div`
   background-color: #fffcef;
 `;
 
-const productsMock = [
-  {
-    photos: [
-      "https://images.unsplash.com/photo-1571654681830-ef991494a42a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=967&q=80",
-    ],
-    description: "preto seminovo",
-    name: "Iphone 11",
-    category: "Eletrônicos",
-    installments: "5",
-    price: "4800",
-    paymentMethod: "Cartao",
-    id: "P8Vji2bRCHisY6oHmmTU",
-  },
-  {
-    paymentMethod: "card",
-    installments: 3,
-    description: "Esse é um produto muito legal!",
-    category: "Categoria 1",
-    price: 10,
-    photos: ["https://picsum.photos/300/200"],
-    name: "Produto",
-    id: "Tlsl8zpFLWGAss9DMwmu",
-  },
-  {
-    description: "nunca usada",
-    photos: [
-      "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-    ],
-    name: "Blusa preta",
-    category: "Roupas",
-    paymentMethod: "Paypal",
-    price: "89",
-    installments: 1,
-    id: "UD8uTnDudBYBv5ev1Edm",
-  },
-  {
-    paymentMethod: "Cartao",
-    name: "Kindle",
-    category: "Eletrônicos",
-    description: "usado 2 meses",
-    price: "289",
-    installments: "4",
-    photos: [
-      "https://images.unsplash.com/photo-1455541504462-57ebb2a9cec1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-    ],
-    id: "d1DUDXtEWAshGdtsdX8Y",
-  },
-];
-
 export class AppContainer extends Component {
   state = {
     page: 1,
     listProducts: [],
-    inputSearch: ""
+    inputSearch: "",
   };
 
   onClickChangePage = (numPage) => {
@@ -153,7 +104,9 @@ export class AppContainer extends Component {
             listProducts={this.state.listProducts}
             onClickChangePage={this.onClickChangePage}
           />
-          {this.state.page === 1 && <ProductsPage inputSearch={this.state.inputSearch} />}
+          {this.state.page === 1 && (
+            <ProductsPage inputSearch={this.state.inputSearch} />
+          )}
           {this.state.page === 2 && <PageSeller />}
           <Footer />
         </Container>

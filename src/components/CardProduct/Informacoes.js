1109b459b9cdab3837d63781bb0966335fee92e6
GuilhermeSoutello <guisoutello@gmail.com>
2021-04-03 18:49:13 -0300

    informacao produto finalizado

---
+++ src/components/CardProduct/Informacoes.js
@@ -0,0 +1,50 @@
+import {transformToReal} from '../../utils/transformToReal'
+import React from 'react'
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  Button,
+  Portal,
+} from "@chakra-ui/react"
+
+export default function Informacoes (props) {
+
+return (<Popover>
+        <PopoverTrigger>
+        <Button 
+        bg='#EF4242'
+        color="#FFFFFF"
+        _hover={{ bg: "#F56565" }}
+        marginTop="25px">Saiba Mais</Button>
+  </PopoverTrigger>
+  <Portal>
+    <PopoverContent>
+      <PopoverArrow />
+      <PopoverHeader>{props.categoria}</PopoverHeader>
+      <PopoverCloseButton />
+      <PopoverBody>
+        <p>Descrição: {props.descricao}</p>
+        <p>Preço: R${transformToReal(props.preco)}</p>
+        <p>Método de Pagamento: {props.metododepagamento}</p>
+        <p>Parcelamento: {props.parcelamento}x</p>
+      </PopoverBody>
+      <PopoverFooter>
+        <Button
+        onClick={() => props.adicionarproduto(props.item)}
+        bg='#EF4242'
+        color="#FFFFFF"
+        _hover={{ bg: "#F56565" }}
+        marginTop="25px" >Adicionar ao Carrinho</Button>
+
+      </PopoverFooter>
+    </PopoverContent>
+  </Portal>
+</Popover>)
+
+}
